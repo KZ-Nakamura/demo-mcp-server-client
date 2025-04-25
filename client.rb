d34ce944c9eed093b58c8be0b425a3091022bb29
razokulover <yuhei.nakasaka@gmail.com>
2025-04-26 01:05:55 +0900

    CLIの基礎

--- client.rb
+++ client.rb
@@ -1,4 +1,18 @@
-#! /usr/bin/env ruby
+def process_query(query)
+  puts "process #{query}"
+end
+
+def chat_loop
+  puts 'MCP Client Started!'
+  puts "Type your queries 'exit' to exit."
+
+  loop do
+    input = $stdin.gets.chomp
+    break if input.downcase == 'exit'
+
+    process_query(input)
+  end
+end
 
 file_path = ARGV[0]
 
@@ -7,4 +21,4 @@ if file_path.nil?
   exit
 end
 
-puts "executing #{file_path}"
+chat_loop
